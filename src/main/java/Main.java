065f92fad2948c8d9c94c4dbd701c5e1a1c31f0c
Kevis <kevis@Zipcoders-MacBook-Pro-82.local>
2023-10-29 20:17:57 -0400

    All done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,25 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
 
-    }
+        Scanner scanner = new Scanner(System.in);
+
+
+        String name = "";
+
+        if(name.equals("Alice")){
+            System.out.print("Hello Alice");
+        }
+        else if(name.equals("Bob")) {
+            System.out.print("Hello Bob");
+
+        } else{System.out.print("You are neither Alice or Bob");
+
+        }
+
+    }//I did it
 }
